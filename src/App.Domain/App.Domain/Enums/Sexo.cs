7830aa778893d0518e7d323195d894b5ed26c9b3
marcosfreire <marcos.freire@zarpsystem.com.br>
2020-02-11 19:53:46 -0300

    incluido testes unitarios para classe Cliente

---
+++ src/App.Domain/App.Domain/Enums/Sexo.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Domain.Enums
+{
+    public enum Sexo
+    {
+        Masculino = 1,
+        Feminimo = 2
+    }
+}
---
+++ src/App.Domain/App.Domain/Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using App.Domain.Enums;
+using System;
+
+namespace App.Domain.Models
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Sexo Sexo { get; set; }
+        public bool Ativo { get; set; }
+
+        public Cliente(Guid id, string nome, string sobreNome, DateTime dataNascimento, Sexo sexo, bool ativo)
+        {
+            Id = id;
+            Nome = nome;
+            SobreNome = sobreNome;
+            DataNascimento = dataNascimento;
+            Sexo = sexo;
+            Ativo = ativo;
+        }
+
+        public bool Valido()
+        {
+            var valido = Id != Guid.Empty;
+            valido &= !string.IsNullOrEmpty(Nome);
+            valido &= !string.IsNullOrEmpty(SobreNome);
+            valido &= DataNascimento < DateTime.Now.AddYears(-18);
+
+            return valido;
+        }
+    }
+}
